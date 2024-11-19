05375e2e20059ea97c67db046f937cf8f1537075
martesytrece <127934236+martesytrece@users.noreply.github.com>
2024-11-19 20:02:31 +0100

    Update game.js
    Se ha creado una clase adicional Player para controlar el movimiento de los jugadores y depurar la clase Game

--- src/game.js
+++ src/game.js
@@ -1,10 +1,11 @@
+import Player from './player.js';
+
 class Game extends Phaser.Scene
 {
     constructor()
 	{
 		super('Game');
 	}
-
 
     init(data)
 	{
@@ -15,63 +16,29 @@ class Game extends Phaser.Scene
 	{
         //Placeholders
 		this.load.image('cat', '../assets/cat.png');
+        this.load.spritesheet('frank', '../assets/francat_spritesheet.png', { frameWidth: 28*4, frameHeight: 32*4 });
 	}
 
     
 	create(data)
 	{ 
-        this.player1 = this.physics.add.sprite(100, 450, 'cat');
-        this.keys = [
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A), 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S), 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D) 
-        ]
+        this.keys1 = [ "W", "A", "S", "D" ]
+        this.player1 = this.physics.add.existing(new Player(this, 50, 100, 'frank', 5, this.keys1));
+        this.keys2 = [ "UP", "LEFT", "DOWN", "RIGHT" ]
+        this.player2 = this.physics.add.existing(new Player(this, 450, 100, 'frank', 5, this.keys2));
 
-        this.player2 = this.physics.add.sprite(500, 450, 'cat');
-        this.cursors = [
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP),
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT), 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN), 
-            this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT) 
-        ]
-        this.collission = this.physics.add.collider(this.player1, this.player2);
+        this.collission = this.physics.add.collider(this.player1.sprite, this.player2.sprite);
     }
+
+
+
 
 	update(time, delta)
 	{
-		this.updatePos(this.player1, this.keys);
-        this.updatePos(this.player2, this.cursors);
-       
+       this.player1.updateMovement();
+       this.player2.updateMovement();
 	}
 
-    updatePos(player, vec){
-        var velocity = 200;
-
-        if (vec[0].isDown){
-            player.setVelocityX(0);
-            player.setVelocityY(-velocity);
-        }
-        else if (vec[1].isDown)
-        {
-            player.setVelocityX(-velocity);
-            player.setVelocityY(0);
-            //this.anims.play('left', true);
-        } 
-        else if (vec[2].isDown){
-            player.setVelocityX(0);
-            player.setVelocityY(velocity);
-        }
-        else if (vec[3].isDown)
-        {
-            player.setVelocityX(velocity);
-            player.setVelocityY(0);
-        }      
-        else {
-            player.setVelocityX(0);
-            player.setVelocityY(0);
-        }
-    }
 }
 
 export default Game;
