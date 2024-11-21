2bc0360c7e10f4220176a9421196c7657559d15b
SaraRomBer <127934149+SaraRomBer@users.noreply.github.com>
2024-11-21 13:18:43 +0100

    Arreglo de fallos
    Se han arreglado algunos fallos que se han cometido al juntar ramas.

--- src/game.js
+++ src/game.js
@@ -23,23 +23,26 @@ class Game extends Phaser.Scene {
 
 	create(data)
 	{ 
-	      this.player1 = this.physics.add.existing(new Player(this, 50, 100, 'frank', 1, this.keys1, "0xfad927"));
+		this.grid = new Grid(this,'box');
+        this.keys1 = [ "W", "A", "S", "D", "E"];
+	    
+		this.player1 = this.physics.add.existing(new Player(this, 50, 100, 'frank', 1, this.keys1, "0xfad927"));
         this.velocity1 = 200;
         this.player1.setCollideWorldBounds(true);
 
         //Player 2 Configuration
-        this.keys2 = [ "UP", "LEFT", "DOWN", "RIGHT", "SHIFT"]
+        this.keys2 = [ "UP", "LEFT", "DOWN", "RIGHT", "SHIFT"];
         this.player2 = this.physics.add.existing(new Player(this, 450, 100, 'agata', 2, this.keys2, "0xfa27ba"));
         this.velocity2 = 200;
         this.player2.setCollideWorldBounds(true);
         
-        this.collission = this.physics.add.collider(this.player1.sprite, this.player2.sprite);
+        //this.collission = this.physics.add.collider(this.player1.sprite, this.player2.sprite);
     	}
 
 	update(time, delta)
   {
-	  	  this.grid.updateGrid(this.player1);
-		    this.grid.updateGrid(this.player2);
+	  	this.grid.updateGrid(this.player1);
+		this.grid.updateGrid(this.player2);
     
         // Update both players basic movement
         this.player1.updateMovement(this.velocity1);
