7de98d8c21bdb98963470e14df068a36b0f709d6
martesytrece <127934236+martesytrece@users.noreply.github.com>
2024-11-21 16:50:34 +0100

    Update player.js
    Funciones para controlar los power ups

--- src/player.js
+++ src/player.js
@@ -1,8 +1,6 @@
-import Game from './game.js';
-
 class Player extends Phaser.Physics.Arcade.Sprite {
-
-    constructor(scene, x, y, texture, number, keys, color) {
+   
+    constructor(scene, x, y, texture, number, keys, velocity, color){
         super(scene, x, y, 'Texture');
 
         this.scene = scene;
@@ -10,52 +8,50 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.y = y;
         this.texture = texture;
 
-        this.color = color;
-
         this.name = `Player ${number}`;
-        this.life = 4;
+        this.color = color;
+        this.initialVelocity = velocity;
+        this.velocity = velocity;
+        this.lifes = 4;
         this.deaths = 0;
         this.addKeys(keys);
         this.sprite = this.scene.physics.add.sprite(this.x, this.y, this.texture);
         this.sprite.depth = 1;
-
-        this.direction = this.getInitialDirection(number);
-
+        this.direction;
+        this.getInitialDirection(number);
         this.createAnimations();
+
         this.isAnimating = false;
         this.isAttacking = false;
 
         console.log(`${this.name} currently playing ${this.texture}: created successfully!`);
         console.log(`Using controls ${keys}`);
 
-        // Manage animations
+        // Handle the Animation timing
+        this.power = false; // If Player holds Stamina PowerUp
         this.cont = 0;
         this.clock = 0;
         this.respawnclock = 0;
     }
 
-    addKeys(keys) {
+    // Create PLayer configuration
+    addKeys(keys){
         this.controls = [];
-        for (var i = 0; i < keys.length; i++) {
-            this.controls.push(this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes[keys[i]]));
-        }
+        for (var i = 0; i < keys.length; i++){
+            this.controls.push( this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes[keys[i]]));
+        }    
     }
 
-    getInitialDirection(n) {
-        if ((n - 1) % 2 == 0) {
-            return 'right';
-        }
-        else {
-            return 'left';
-        }
+    getInitialDirection(number){
+        this.direction = (number-1) % 2 == 0 ? 'right' : 'left';
     }
 
-    createAnimations() {
+    createAnimations(){
         this.scene.anims.create({
             key: `${this.texture}-idle`,
             frames: this.scene.anims.generateFrameNumbers(`${this.texture}`, { start: 0, end: 5 }),
             frameRate: 10,
-            repeat: -1
+            repeat: 0
         });
 
         this.scene.anims.create({
@@ -75,7 +71,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.scene.anims.create({
             key: `${this.texture}-powerup`,
             frames: this.scene.anims.generateFrameNumbers(`${this.texture}`, { start: 24, end: 29 }),
-            frameRate: 10,
+            frameRate: 14,
             repeat: 0
         });
 
@@ -102,124 +98,137 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     }
 
     // Special animations
-    runAnimation(player, animation, x, y) {
+    runAnimation(player, animation, x, y){
         player.isAnimating = true;
         player.sprite.setVelocityX(x);
         player.sprite.setVelocityY(y);
-        player.sprite.play(animation, true);
+        this.checkDirection();
+        player.sprite.play(animation, true);                   
         player.sprite.on('animationcomplete', () => {
             player.isAnimating = false;
-        });
+        });  
     }
 
     // Basic Movement
-    runMovement(animation, x, y) {
+    runMovement(animation, x, y){
         this.checkDirection();
         this.sprite.setVelocityX(x);
         this.sprite.setVelocityY(y);
-        this.sprite.anims.play(animation, true);
+        this.sprite.anims.play(animation, true);  
     }
 
-    checkDirection() {
-        if (this.direction == 'left') {
-            this.sprite.flipX = true;
-        }
-        else {
-            this.sprite.flipX = false;
-        }
+    checkDirection(){
+        this.sprite.flipX = this.direction == 'left' ? true : false;   
     }
 
-    updateMovement(velocity) {
+    updateMovement(velocity){
         // Attack animation
-        if (this.controls[4].isDown && !this.isAttacking) {
-            this.isAttacking = true;
-            this.runAnimation(this, `${this.texture}-attack`, 0, 0);
+        if(this.controls[4].isDown && this.isAttacking && this.power){  
+            this.runAnimation(this, `${this.texture}-attack`, 0, 0);                  
         }
-        else if (this.controls[4].isUp && this.isAttacking) {
+        else if(this.controls[4].isDown && !this.isAttacking && !this.isAnimating){
+            this.isAttacking = true;  
+            this.runAnimation(this, `${this.texture}-attack`, 0, 0);                  
+        }
+        else if(this.controls[4].isUp && this.isAttacking && !this.power){
             this.isAttacking = false;
         }
 
         // If not busy doing other animations
-        if (!this.isAnimating) {
+        if(!this.isAnimating){
             // Up Movement
-            if (this.controls[0].isDown) {
+            if (this.controls[0].isDown){               
                 this.runMovement(`${this.texture}-move`, 0, -velocity);
             }
             // Left movement
             else if (this.controls[1].isDown) {
                 this.runMovement(`${this.texture}-move`, -velocity, 0);
                 this.direction = 'left';
-            }
+            } 
 
             // Down movement
-            else if (this.controls[2].isDown) {
+            else if (this.controls[2].isDown){
                 this.runMovement(`${this.texture}-move`, 0, velocity);
             }
             // Right Movement
             else if (this.controls[3].isDown) {
                 this.runMovement(`${this.texture}-move`, velocity, 0);
                 this.direction = 'right';
-            }
+            }          
             // If player does not click any key
             else {
                 this.runMovement(`${this.texture}-idle`, 0, 0);
             }
-        }
+        }       
     }
 
-    checkCollission(other, delta) {
+    checkCollission(other, delta){
         this.clock += delta;
-        this.clockRestart(delta);
+        this.clockHandling(delta);
         this.updateCollission(other, delta);
     }
 
-    clockRestart(delta) {
-        if (this.clock > 200) {
+    clockHandling(delta){
+        if(this.power) {
+            this.clock = 0;
+            this.cont = 0;
+        }
+        else if(!this.power && this.clock > 200){
             this.cont = 0;
             this.clock = 0;
         }
-        while (this.controls[4].isDown && this.clock > 0) {
+        while(this.controls[4].isDown && this.clock > 0 && !this.power){
             this.clock -= delta;
-        }
+        } 
     }
 
-    updateCollission(other, delta) {
-        var distance = this.manhattanDistance(this.sprite.x, this.sprite.y, other.sprite.x, other.sprite.y);
-        if (distance < 85 && this.isAttacking && this.cont < 1) {
-            this.runAnimation(other, `${other.texture}-hit`, 0, 0);
-            console.log(`${this.name} attacked ${other.name}`);
+    updateCollission(other, delta){
+        var distance = this.chevyshevDistance(this.sprite.x, this.sprite.y, other.sprite.x, other.sprite.y);
+
+        if(distance < 80 && this.isAttacking && this.cont < 1){
+            this.runAnimation(other,`${other.texture}-hit`, 0, 0);
+            console.log(`${this.name} attacked ${other.name}`);      
             this.cont++;
-            if (this.cont == 1) {
-                other.life--;
+            if(this.cont >= 1){
+                other.lifes--;
                 this.isAttacking = false;
-                this.clock = 0;
+                this.clock = 0;   
             }
-            console.log(other.life);
-        }
-
-        if (other.life <= 0) {
-            other.respawnclock += delta;
-            if (other.respawnclock > 1 && other.respawnclock < 400) {
-                this.runAnimation(other, `${other.texture}-hit`, 0, 0);
+        }  
+        
+        // If Player kills other player
+        if (other.lifes <= 0) { 
+            other.respawnclock += delta;           
+            if(this.power){
+                this.deathSequence(other, 400, 1200);
             }
-            if (other.respawnclock > 800) {
-                this.runAnimation(other, `${other.texture}-die`, 0, 0);
-            }
-
-            if (other.respawnclock > 1800) {
-                other.sprite.x = other.x;
-                other.sprite.y = other.y;
-                this.runAnimation(other, `${other.texture}-teleport`, 0, 0);
-                other.life = 4;
-                other.respawnclock = 0;
-                other.deaths++;
-                console.log(other.deaths);
+            else {
+                this.deathSequence(other, 800, 1800);
             }
         }
     }
 
-    manhattanDistance(x1, y1, x2, y2) {
-        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    deathSequence(other, a, b){
+        if(other.respawnclock > 1 && other.respawnclock < 400){
+            this.runAnimation(other,`${other.texture}-hit`, 0, 0);  
+        }
+        if (other.respawnclock > a ) {   
+            this.runAnimation(other,`${other.texture}-die`, 0, 0);                     
+        }
+        if(other.respawnclock > b ) {
+            other.sprite.x = other.x;
+            other.sprite.y = other.y;
+            this.runAnimation(other,`${other.texture}-teleport`,0, 0);
+            other.lifes = 4;   
+            other.respawnclock = 0;    
+            other.deaths++;
+            console.log(other.deaths);      
+        } 
+    }
+
+    chevyshevDistance(x1, y1, x2, y2){
+        return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
+        //return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 }
 
