d3b111df97bea59d1a1ae724ad5c3d5282502cfc
Samuelovich <samuelretamero@outlook.com>
2024-11-25 15:40:31 +0100

    Tiempo de juego

--- src/game.js
+++ src/game.js
@@ -13,12 +13,21 @@ class Game extends Phaser.Scene {
 
         this.player1_ink = this.getPlayerColor(this.player1_character);
         this.player2_ink = this.getPlayerColor(this.player2_character);
+
+<<<<<<< HEAD
+        this.setTimeStart = 1;
+        
+=======
+>>>>>>> fe90c31ad685f1de8a338ef7f44d48027faba3ae
+		this.over = false;
     }
 
     preload() {
         // Placeholders
         this.load.image('cat', '../assets/cat.png');
         this.load.image('box', '../assets/box.png');
+
+		this.load.image('endcard', '../assets/ui/spr_endcard_back.png');
 
         this.load.image('ink', '../assets/ink.png');
 
@@ -31,6 +40,10 @@ class Game extends Phaser.Scene {
     create(data) {
         // World Configuration
         this.grid = new Grid(this, 'ink');
+
+
+        this.initTime = 0;
+        this.gameDuration = 15;
 
         // Player 1 Configuration
         this.keys1 = ["W", "A", "S", "D", "E"]
@@ -58,11 +71,43 @@ class Game extends Phaser.Scene {
         this.title1 = this.add.text(300, 50, ' ', { color: '#E5B770', fontSize: '96px', fontFamily: 'Metamorphous' });
         this.title2 = this.add.text(300, 150, ' ', { color: '#E5B770', fontSize: '96px', fontFamily: 'Metamorphous' });
 
+<<<<<<< HEAD
+        this.timeText = this.add.text(600, 150, ' ', { color: '#E5B770', fontSize: '96px', fontFamily: 'Metamorphous' });
+        
+=======
+>>>>>>> fe90c31ad685f1de8a338ef7f44d48027faba3ae
+		this.endcard_upper = this.add.image(-922, 1117, 'endcard');
+		this.endcard_upper.angle = -28.6;
+		this.endcard_upper.depth = 10;
+
+		this.endcard_text_upper = this.add.text(-1015.7, 1140.3, "Game's", { color: '#452600', fontSize: '30pt', fontFamily: 'Metamorphous' });
+		this.endcard_text_upper.angle = -28.6;
+		this.endcard_text_upper.depth = 10;
+
+		this.endcard_lower = this.add.image(2203, -463, 'endcard');
+		this.endcard_lower.angle = -28.6;
+		this.endcard_lower.depth = 10;
+
+		this.endcard_text_lower = this.add.text(2193, -481.1, "Over!", { color: '#452600', fontSize: '30pt', fontFamily: 'Metamorphous' });
+		this.endcard_text_lower.angle = -28.6;
+		this.endcard_text_lower.depth = 10;
+
     }
 
     update(time, delta) {
-        this.timer = time / 1000; //Time in seconds
-        if (this.timer < 15) {
+        console.log('setTimeStart: ' + this.setTimeStart);
+        console.log('initTime: ' + this.initTime);
+        console.log('Time: ' +time);
+        if(this.initTime == 0){
+        this.initTime = this.setInitTime(time);
+        }
+        console.log('initTime: ' + this.initTime);
+        console.log('Time: ' +time);
+        this.timer = (time / 1000) - this.initTime;
+
+        this.timeText.setText(Math.round(this.timer));
+
+        if (this.timer < this.gameDuration) {
             console.log('Time: ' +time);
             this.countDown(this.timer, this.title1);
             if (this.timer < 2) {
@@ -97,9 +142,16 @@ class Game extends Phaser.Scene {
                 this.powerup2.updatePowerup(this.player2, delta, this.grid);
                 this.powerup3.updatePowerup(this.player2, delta, this.grid);
             }
-        } else {
+        } 
+		else if(!this.over)
+		{
+			this.over = true;
             this.finishGame();
         }
+    }
+
+    setInitTime(time){
+        return time / 1000;
     }
 
     getPlayerColor(character) {
@@ -114,11 +166,13 @@ class Game extends Phaser.Scene {
         return "0xFFFFFF";
     }
 
-    finishGame() {
-        let ranking = this.grid.countColors();
-        this.scene.start('Endgame', [ranking,this.player1,this.player2]); 
+	toResults()
+	{
+		let scene = this.parent.scene;
 
-    }
+		let ranking = scene.grid.countColors();
+        scene.scene.start('Endgame', [ranking, scene.player1, scene.player2]); 
+	}
 
     countDown(timer, title) {
         if (timer < 5) {
@@ -126,7 +180,7 @@ class Game extends Phaser.Scene {
         }
         else if (timer >= 6 && timer < 7) {
             title.setText('GO!');
-
+            
         }
         else if (timer >= 7) {
             title.visible = false;
@@ -144,6 +198,95 @@ class Game extends Phaser.Scene {
             default:
         }
     }
+
+	// End sequence ------------------------------------
+	finishGame() 
+	{
+		console.log("Game Over");
+        this.add.tween({
+			targets: this.endcard_upper,
+			duration: 1000,
+			x: 614,
+			y: 280,
+			ease: 'Cubic.inOut'
+		});
+
+		this.add.tween({
+			targets: this.endcard_text_upper,
+			duration: 1000,
+			x: 520.3,
+			y: 303.3,
+			ease: 'Cubic.inOut'
+		});
+
+		this.add.tween({
+			targets: this.endcard_lower,
+			duration: 1000,
+			x: 665.6,
+			y: 374,
+			ease: 'Cubic.inOut'
+		});
+
+		this.add.tween({
+			targets: this.endcard_text_lower,
+			duration: 1000,
+			x: 655.6,
+			y: 355.9,
+			ease: 'Cubic.inOut',
+			onComplete: this.idleBars
+		});
+    }
+
+	idleBars()
+	{
+		let scene = this.parent.scene;
+
+		// Using a tween as a timer
+		scene.add.tween({
+			targets: scene.endcard_text_upper,
+			duration: 1000,
+			x: '+=0',
+			onComplete: scene.removeBars
+		});
+	}
+
+	removeBars()
+	{	
+		let scene = this.parent.scene;
+
+		scene.add.tween({
+			targets: scene.endcard_upper,
+			duration: 1000,
+			x: 2150,
+			y: -557,
+			ease: 'Cubic.inOut'
+		});
+
+		scene.add.tween({
+			targets: scene.endcard_text_upper,
+			duration: 1000,
+			x: 2056.3,
+			y: -533.7,
+			ease: 'Cubic.inOut'
+		});
+
+		scene.add.tween({
+			targets: scene.endcard_lower,
+			duration: 1000,
+			x: -871.8,
+			y: 1211,
+			ease: 'Cubic.inOut'
+		});
+
+		scene.add.tween({
+			targets: scene.endcard_text_lower,
+			duration: 1000,
+			x: -881.8,
+			y: 1192.9,
+			ease: 'Cubic.inOut',
+			onComplete: scene.toResults
+		});
+	}
 }
 
 export default Game;
