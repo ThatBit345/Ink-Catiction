5cb8def0552047f1fa044abc2e544294f9e5cd42
SaraRomBer <127934149+SaraRomBer@users.noreply.github.com>
2024-12-18 15:14:24 +0100

    Codificación de contraseñas
    Se ha implementado la codificación de las contraseñas.

--- src/main/java/com/momostudios/ink_catiction/InkCatictionApplication.java
+++ src/main/java/com/momostudios/ink_catiction/InkCatictionApplication.java
@@ -3,17 +3,18 @@ package com.momostudios.ink_catiction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @SpringBootApplication
 public class InkCatictionApplication {
 
-	/*
-	// Para que las dependencias se inyecten solas
 	@Bean
-	public ApiStatusService getStatusService() {
-	
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers(
+		new AntPathRequestMatcher("/**")
+		);
 	}
-	*/
 
 	public static void main(String[] args) {
 		System.out.println( "Working dir: " + System.getProperty("user.dir"));
--- src/main/java/com/momostudios/ink_catiction/UserController.java
+++ src/main/java/com/momostudios/ink_catiction/UserController.java
@@ -18,10 +18,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
 
     @Autowired
     private UserDAO userDAO;
@@ -29,8 +31,11 @@ public class UserController {
     @Autowired
     private ApiStatusService apiStatusService;
 
+    BCryptPasswordEncoder encoder;
+
     public UserController(UserDAO userDAO) {
         this.userDAO = userDAO;
+        this.encoder = new BCryptPasswordEncoder(16);
     }
 
     /**
@@ -61,6 +66,7 @@ public class UserController {
      * @param password
      * @return
      */
+    // Debería ser un post
     @GetMapping("/{username}/{password}")
     public ResponseEntity<UserDTO> logIn(@PathVariable String username, @PathVariable String password) {
         synchronized (this.userDAO) {
@@ -69,7 +75,8 @@ public class UserController {
 
             if (user.isPresent()) 
 			{
-				if(user.get().getPassword().equals(password)){
+                boolean success = encoder.matches(password, user.get().getPassword());
+				if(success){
 					return ResponseEntity.ok(new UserDTO(user.get()));
 				} else {
 					return ResponseEntity.badRequest().build();
@@ -111,13 +118,15 @@ public class UserController {
             return ResponseEntity.badRequest().build();
         }
 
+        String encodedPassword = encoder.encode(user.getPassword());
+
         synchronized (this.userDAO) {
             Optional<User> other = this.userDAO.getUser(user.getUsername());
             if (other.isPresent()) {
                 return ResponseEntity.status(HttpStatus.CONFLICT).build();
             }
-
 			System.out.println("Registered new user: " + user.getUsername());
+            user.setPassword(encodedPassword);
             this.apiStatusService.hasSeen(user.getUsername());
             this.userDAO.updateUser(user);
             return ResponseEntity.noContent().build();
@@ -138,13 +147,15 @@ public class UserController {
 			return ResponseEntity.badRequest().build();
         }
 
+        String encodedPassword = encoder.encode(passwordUpdate.password);
+
         synchronized (this.userDAO) {
             this.apiStatusService.hasSeen(username);
 
             Optional<User> optionalUser = this.userDAO.getUser(username);
             if (optionalUser.isPresent()) {
                 var user = optionalUser.get();
-                user.setPassword(passwordUpdate.password);
+                user.setPassword(encodedPassword);
                 this.userDAO.updateUser(user);
                 return ResponseEntity.noContent().build();
 
